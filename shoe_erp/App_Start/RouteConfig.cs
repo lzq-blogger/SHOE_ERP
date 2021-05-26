9fe579e22b3527752ee341d1ed1bfba7746650ca
lzq <20000209@qq.com>
2021-05-26 23:22:11 +0800

    5.26

--- shoe_erp/App_Start/RouteConfig.cs
+++ shoe_erp/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace shoe_erp
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "buy_Material", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
