47cb6ab78eebaf45c70ce6e7d978a5a33774de40
lzq <20000209@qq.com>
2021-05-26 23:24:03 +0800

    5.26

---
+++ shoe_erp/Controllers/buy_MaterialController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace shoe_erp.Controllers
+{
+    public class buy_MaterialController : Controller
+    {
+        // GET: buy_Material
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
