3cf9531d23a13ba00fc9234e69ed0b0232ad8277
lzq <20000209@qq.com>
2021-05-31 19:36:55 +0800

    5.31lzq

--- shoe_erp/Controllers/customerController.cs
+++ shoe_erp/Controllers/customerController.cs
@@ -8,8 +8,41 @@ namespace shoe_erp.Controllers
 {
     public class customerController : Controller
     {
-        // GET: customer
-        public ActionResult Index()
+        // GET: customer  一级界面
+        //客户信息
+        public ActionResult customerInfo()
+        {
+            return View();
+        }
+        //客户订单
+        public ActionResult customerOrder()
+        {
+            return View();
+        }
+        //客户订单统计
+        public ActionResult customerOrder_statistical()
+        {
+            return View();
+        }
+
+        //二级界面========================================
+        //查询用户信息
+        public ActionResult select_customerInfo()
+        {
+            return View();
+        }
+        //新增用户信息
+        public ActionResult add_customerInfo()
+        {
+            return View();
+        }
+        //查询客户订单
+        public ActionResult select_customerOrder()
+        {
+            return View();
+        }
+        //新增客户订单
+        public ActionResult add_customerOrder()
         {
             return View();
         }
