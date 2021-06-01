952ea04a78ca5fd0ea899810ccb1a1d5959f0bab
lzq <20000209@qq.com>
2021-06-01 22:58:36 +0800

    6.1最终版

--- shoe_erp/Controllers/productController.cs
+++ shoe_erp/Controllers/productController.cs
@@ -19,12 +19,39 @@ namespace shoe_erp.Controllers
         {
             return View();
         }
+        //领料管理
+        public ActionResult get_materials()
+        {
+            return View();
+        }
         //二级界面====================================
+        //查询生产计划
         public ActionResult select_product_plan()
         {
             return View();
         }
+        //新增生产计划
         public ActionResult add_product_plan()
+        {
+            return View();
+        }
+        //查询领料单
+        public ActionResult select_get_materials_order()
+        {
+            return View();
+        }
+        //新增领料单
+        public ActionResult add_get_materials_order()
+        {
+            return View();
+        }
+        //查询产品生产
+        public ActionResult select_product_product()
+        {
+            return View();
+        }
+        //新增产品生产
+        public ActionResult add_product_product()
         {
             return View();
         }
---
+++ shoe_erp/Controllers/saleController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace shoe_erp.Controllers
+{
+    public class saleController : Controller
+    {
+        // 一级页面
+        //客户订单管理
+        public ActionResult customer_order()
+        {
+            return View();
+        }
+        //销售订单详情
+        public ActionResult sale_order()
+        {
+            return View();
+        }
+        //销售统计
+        public ActionResult sale_statistics()
+        {
+            return View();
+        }
+        //二级页面====================================================
+        //查询客户订单  （这里面有出货功能)
+        public ActionResult select_customer_order()
+        {
+            return View();
+        }
+        //查询销售订单详情 
+        public ActionResult select_sale_order()
+        {
+            return View();
+        }
+    }
+}
