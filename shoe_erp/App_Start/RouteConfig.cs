bdbeb131c2b110ddb6b73b743a4f529490b127cd
lzq <20000209@qq.com>
2021-06-03 20:36:00 +0800

    6.3换模板的

--- shoe_erp/App_Start/RouteConfig.cs
+++ shoe_erp/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace shoe_erp
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "indexmax", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "index", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
