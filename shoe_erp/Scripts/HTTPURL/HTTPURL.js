b96a8721927e9ac1e47b1674643dce675987b91b
lzq <20000209@qq.com>
2021-07-21 18:25:35 +0800

    7.21lzq不干涉pjt的最后提交

--- shoe_erp/Scripts/HTTPURL/HTTPURL.js
+++ shoe_erp/Scripts/HTTPURL/HTTPURL.js
@@ -2,5 +2,5 @@
 var Head_Url = "http://www.gdzweb.cn:8082";
 /*https://localhost:44360/api*/
 /*http://www.gdzweb.cn:8082/api*/
-/*http://106.52.105.93:8082/api*/
-var Head_Url_Api = "http://106.52.105.93:8081/api";
+/*http://106.52.105.93:8081/api*/
+var Head_Url_Api = "https://localhost:44360/api";
